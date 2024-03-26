e99b681b2f47a1241808eb8f7d3a91d23e246686
Ljupche <baldovaliev.b@gmail.com>
2024-03-26 22:45:08 +0100

    Initialize Spring Boot project

---
+++ src/main/kotlin/com/example/tsguru/TsGuruApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tsguru
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TsGuruApplication
+
+fun main(args: Array<String>) {
+    runApplication<TsGuruApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tsguru/TsGuruApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tsguru
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TsGuruApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
